0bfbd1e8cf78b2d646efb4e206c25e6aab294f25
abhisek <a.maiti@utwente.nl>
2022-06-15 17:22:43 +0200

    fix script

--- run.sh
+++ run.sh
@@ -1,21 +1,20 @@
 #!/usr/bin/env bash
 # TODO: log clean up and other housekeeping
-for i in "$@"; do
-  case $i in
-    -h=*|--host=*)
-      HOST="${i#*=}"
-      shift
-      ;;
-    -p=*|--port=*)
-      PORT="${i#*=}"
-      shift
-      ;;
-    *)
-      ;;
+options=$(getopt -a -n run -o p:h: --long port:,host: -- "$@")
+eval set -- "$options"
+while :
+do
+  case "$1" in
+    -p | --port) PORT="$2" ; shift 2 ;;
+    -h | --host) HOST="$2" ; shift 2 ;;
+             --) shift;        break ;;
+    *) echo "Unexpected option: $1 - this should not happen." ;;
   esac
 done
 rm -rf logs/FloodNet/*
 micromama activate PySeg
 python main.py &
 tensorboard --logdir logs/FloodNet --port "${PORT}" --host "${HOST}" &
+
+wait
 echo "🏁 Complete 🏁"
