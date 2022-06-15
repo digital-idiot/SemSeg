0b428eafae3d5727ca8531519fdb2741874b6141
abhisek <a.maiti@utwente.nl>
2022-06-15 16:26:21 +0200

    script

--- run.sh
+++ run.sh
@@ -1,2 +1,21 @@
 #!/usr/bin/env bash
 # TODO: log clean up and other housekeeping
+for i in "$@"; do
+  case $i in
+    -h=*|--host=*)
+      HOST="${i#*=}"
+      shift
+      ;;
+    -p=*|--port=*)
+      PORT="${i#*=}"
+      shift
+      ;;
+    *)
+      ;;
+  esac
+done
+rm -rf logs/FloodNet/*
+micromama activate PySeg
+python main.py &
+tensorboard --logdir logs/FloodNet --port "${PORT}" --host "${HOST}" &
+echo "🏁 Complete 🏁"
